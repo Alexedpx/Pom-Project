cdcc6aaa22ae5c0e12225b0b24ec12734c569533
AlexeDpx <alexandradepourtoux93@gmail.com>
2024-12-02 16:26:04 +0100

    💄  in progress, update ui of component Choices

--- apps/webapp/src/components/features/Choices.tsx
+++ apps/webapp/src/components/features/Choices.tsx
@@ -1,16 +1,34 @@
-import Layout from "./Layout";
+import { useTranslation } from 'react-i18next';
+import { Button } from '../ui/button'
+import Layout from './Layout';
+
 
 function Choices() {
-
+    const { t } = useTranslation('common', {
+        keyPrefix: 'Choices',
+    })
     return (
 
         <>
-
             <Layout>
                 <Layout.Header>
-                    <h1>Hello</h1>
+                    <h1 className='font-bold text-4xl'>{t('hello')}</h1>
                 </Layout.Header>
-            </Layout>
+                <Layout.Content>
+                    <h2 className='font-bold text-3xl'>{t('howDoYouFeelToday')}</h2>
+                </Layout.Content>
+                <Layout.Footer>
+                    <div className='border-2 border-violet-500 flex justify-evenly'>
+                        <Button variant='icon' size='lg'></Button>
+                        <Button variant='icon' size='lg'></Button>
+                        <Button variant='icon' size='lg'></Button>
+                        <Button variant='icon' size='lg'></Button>
+                    </div>
+                    <div className='flex justify-center'>
+                        <p className='text-base'>{t('selectYourMood')}</p>
+                    </div>
+                </Layout.Footer >
+            </Layout >
         </>
     )
 }
--- apps/webapp/src/components/features/Layout.tsx
+++ apps/webapp/src/components/features/Layout.tsx
@@ -2,30 +2,33 @@ import { PropsWithChildren } from "react"
 
 function Layout({ children }: PropsWithChildren) {
     return (
-        <div className="w-full h-full flex">
+        <div className="w-full h-screen text-tertiary-foreground border-2 border-red-500 flex flex-col bg-tertiary">
             {children}
         </div>
     )
 }
 
-function Header() {
+function Header({ children }: PropsWithChildren) {
     return (
-        <div className="w-full">
+        <div className="border-2 border-blue-500 h-1/5  w-full flex justify-center items-center  ">
+            {children}
         </div>
     )
 }
 
-function Content() {
+function Content({ children }: PropsWithChildren) {
     return (
-        <div className="w-full flex flex-grow ">
+        <div className="border-2 border-green-500 flex text-center flex-grow w-full items-center justify-center">
+            {children}
         </div>
     )
 
 }
 
-function Footer() {
+function Footer({ children }: PropsWithChildren) {
     return (
-        <div className="w-full">
+        <div className="border-2 border-pink-500 w-full h-1/4 flex flex-col justify-around">
+            {children}
         </div>
     )
 
