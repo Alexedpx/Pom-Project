dfd6388bfdb87c48157a6be67f9463c4186da6a7
AlexeDpx <alexandradepourtoux93@gmail.com>
2024-12-03 15:19:20 +0100

    ✨ 🎨 add function to validate mood , add toast and navigate to page calendar(recap)

--- apps/webapp/src/components/features/Choices.tsx
+++ apps/webapp/src/components/features/Choices.tsx
@@ -7,27 +7,34 @@ import BgVhappy from '../../assets/background/BgVhappy.png';
 import BgHappy from '../../assets/background/BgHappy.png';
 import BgSad from '../../assets/background/BgSad.png';
 import BgAngry from '../../assets/background/BgAngry.png';
+import { Check } from 'lucide-react';
+import ReactEmojis from "@souhaildev/reactemojis";
+import { toast, Toaster } from 'sonner';
+import { useNavigate } from 'react-router';
+
+type EmojiType = "😀" | "🙂" | "😓" | "😡";
 
 function Choices() {
     const { t } = useTranslation('common', {
         keyPrefix: 'Choices',
     });
+    const navigate = useNavigate()
     const [background, setBackground] = useState('bg-tertiary');
     const [vector, setVector] = useState<string>(BgDefault);
-    const [selectedIcon, setSelectedIcon] = useState<string | null>(null);
+    const [selectedIcon, setSelectedIcon] = useState<EmojiType | null>(null);
 
-    const handleIconClick = (icon: string) => {
+    const handleIconClick = (icon: EmojiType) => {
         setSelectedIcon(icon);
         switch (icon) {
-            case '😃':
+            case '😀':
                 setBackground('bg-veryHappy');
                 setVector(BgVhappy);
                 break;
-            case '😊':
+            case '🙂':
                 setBackground('bg-happy');
                 setVector(BgHappy);
                 break;
-            case '😢':
+            case '😓':
                 setBackground('bg-sad');
                 setVector(BgSad);
                 break;
@@ -44,11 +51,11 @@ function Choices() {
 
     const getButtonVariant = (icon: string | null) => {
         switch (icon) {
-            case '😃':
+            case '😀':
                 return 'veryHappy';
-            case '😊':
+            case '🙂':
                 return 'happy';
-            case '😢':
+            case '😓':
                 return 'sad';
             case '😡':
                 return 'angry';
@@ -59,11 +66,11 @@ function Choices() {
 
     const getMoodText = (text: string | null) => {
         switch (text) {
-            case '😃':
+            case '😀':
                 return t('moods.veryGood');
-            case '😊':
+            case '🙂':
                 return t('moods.good');;
-            case '😢':
+            case '😓':
                 return t('moods.quiteSad');;
             case '😡':
                 return t('moods.angry');;
@@ -72,12 +79,27 @@ function Choices() {
         }
     };
 
+
+    const handleValidate = () => {
+        if (selectedIcon) {
+            localStorage.setItem('userMood', selectedIcon);
+            toast.success('Mood saved !');
+        }
+        setTimeout(() => {
+            navigate('/homepage');
+        }, 1500);
+    };
+    const savedMood = localStorage.getItem('userMood');
+    console.log(savedMood);
+
+
     return (
         <>
+            <Toaster position="top-center" />
             <Layout background={background} vector={vector}>
                 <Layout.Header>
                     {selectedIcon ? (
-                        <h1 className="font-semibold text-4xl">{getMoodText(selectedIcon)}</h1>
+                        <h1 className="font-semibold text-4xl text-white">{getMoodText(selectedIcon)}</h1>
                     ) : (
                         <h1 className="font-semibold text-4xl">{t('header.hello')}</h1>
                     )}
@@ -85,9 +107,9 @@ function Choices() {
                 </Layout.Header>
                 <Layout.Content>
                     {selectedIcon ? (
-                        <div className="text-9xl">{selectedIcon}</div>
+                        <ReactEmojis emoji={selectedIcon} emojiStyle="3" style={{ width: 180, height: 180 }} />
                     ) : (
-                        <h2 className="font-semibold text-3xl">{t('content.howDoYouFeelToday')}</h2>
+                        <h2 className="font-semibold text-2xl">{t('content.howDoYouFeelToday')}</h2>
                     )}
                 </Layout.Content>
                 <Layout.Footer>
@@ -95,39 +117,35 @@ function Choices() {
                         <Button
                             variant="icon"
                             size="icon"
-                            style={{ fontSize: '2rem' }}
-                            onClick={() => handleIconClick('😃')}
+                            onClick={() => handleIconClick('😀')}
                         >
-                            😃
+                            <ReactEmojis emoji='😀' emojiStyle='3' style={{ width: 50, height: 50 }} />
                         </Button>
                         <Button
                             variant="icon"
                             size="icon"
-                            style={{ fontSize: '2rem' }}
-                            onClick={() => handleIconClick('😊')}
+                            onClick={() => handleIconClick('🙂')}
                         >
-                            😊
+                            <ReactEmojis emoji='🙂' emojiStyle='3' style={{ width: 50, height: 50 }} />
                         </Button>
                         <Button
                             variant="icon"
                             size="icon"
-                            style={{ fontSize: '2rem' }}
-                            onClick={() => handleIconClick('😢')}
+                            onClick={() => handleIconClick('😓')}
                         >
-                            😢
+                            <ReactEmojis emoji='😓' emojiStyle='3' style={{ width: 50, height: 50 }} />
                         </Button>
                         <Button
                             variant="icon"
                             size="icon"
-                            style={{ fontSize: '2rem' }}
                             onClick={() => handleIconClick('😡')}
                         >
-                            😡
+                            <ReactEmojis emoji='😡' emojiStyle='3' style={{ width: 50, height: 50 }} />
                         </Button>
                     </div>
                     <div className="flex justify-center ">
                         {selectedIcon ? (
-                            <Button variant={getButtonVariant(selectedIcon)} size='default' className='text-xl font-normal'>{t('buttons.validate')}</Button>
+                            <Button onClick={handleValidate} variant={getButtonVariant(selectedIcon)} size='default' className='text-xl font-normal text-white' iconRight={<Check className="ml-1" />}>{t('button.validate')}</Button>
                         ) : (
                             <p className="text-base">{t('footer.selectYourMood')}</p>
                         )}
